208f53087fa50e563c0f8f36c28730914bf016d4
vitalagency <93239157+vitalagency@users.noreply.github.com>
2025-03-19 22:21:26 -0600

    Controller Route

--- routes/kitchen.js
+++ routes/kitchen.js
@@ -1,7 +1,7 @@
 
 const express = require('express')
 const router = express.Router()
-const controller = require ()
+const controller = require ('../controllers/pos.controller')
 
 // Routers Missing
 module.exports = router
